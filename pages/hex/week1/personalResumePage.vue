57277779d1bdb9b696ecef4facd911bb77386e96
Kerwin <if5400@gmail.com>
2024-07-15 16:05:15 +0800

    update hex page add root tag

--- pages/hex/week1/personalResumePage.vue
+++ pages/hex/week1/personalResumePage.vue
@@ -5,91 +5,93 @@ useSeoMeta({
 </script>
 
 <template>
-<header class="container header">
-  <h1 class="title-1">VITO</h1>
-  <img class="mx-auto" src="/public/hex/week1/personalResume/vito.png" alt="VITO 個人黑白照">
-  <h2 class="ml-auto title-1 w-half-offset">RESUME</h2>
-</header>
-<section class="vito-dec bg-gray-light">
-  <div class="container">
-    <p class="ml-auto w-half-offset fs-1">I'm Vito, a UI Designer with 5+ years of experience. I create visually appealing interfaces for web and mobile apps. Proficient in Sketch, Adobe XD, Figma, HTML, CSS, and JavaScript. I enjoy collaborating with others to create high-quality products.</p>
-  </div>
-</section>
-<main class="container detail">
-  <ul>
-    <li class="mb-10 w-half-offset">
-      <h3 class="mb-4 detail-title title-2">Vito Evans</h3>
-      <address>
+<div class="personalResumePage">
+  <header class="container header">
+    <h1 class="title-1">VITO</h1>
+    <img class="mx-auto" src="/public/hex/week1/personalResume/vito.png" alt="VITO 個人黑白照">
+    <h2 class="ml-auto title-1 w-half-offset">RESUME</h2>
+  </header>
+  <section class="vito-dec bg-gray-light">
+    <div class="container">
+      <p class="ml-auto w-half-offset fs-1">I'm Vito, a UI Designer with 5+ years of experience. I create visually appealing interfaces for web and mobile apps. Proficient in Sketch, Adobe XD, Figma, HTML, CSS, and JavaScript. I enjoy collaborating with others to create high-quality products.</p>
+    </div>
+  </section>
+  <main class="container detail">
+    <ul>
+      <li class="mb-10 w-half-offset">
+        <h3 class="mb-4 detail-title title-2">Vito Evans</h3>
+        <address>
+          <ul class="fs-2">
+            <li class="mb-2">456 Walnut Avenue, Someville, USA</li>
+            <li class="mb-2"><a href="tel:+1 123 456 7890">+1 123 456 7890</a></li>
+            <li class="mb-2"><a href="mailto:info@hexschool.com" target="_blank">info@hexschool.com</a></li>
+            <li><a href="www.infohexschool.com" target="_blank">www.infohexschool.com</a></li>
+          </ul>
+        </address>
+      </li>
+      <li class="mb-10 w-half-offset">
+        <h3 class="mb-4 detail-title title-2">Education</h3>
         <ul class="fs-2">
-          <li class="mb-2">456 Walnut Avenue, Someville, USA</li>
-          <li class="mb-2"><a href="tel:+1 123 456 7890">+1 123 456 7890</a></li>
-          <li class="mb-2"><a href="mailto:info@hexschool.com" target="_blank">info@hexschool.com</a></li>
-          <li><a href="www.infohexschool.com" target="_blank">www.infohexschool.com</a></li>
+          <li class="mb-4">
+            <time class="italic detail-time fs-3 text-gray" datetime="2014-2018">2014-2018</time>
+            <p>Bachelor of Science in Business Administration, University of California, Los Angeles</p>
+          </li>
+          <li class="mb-4">
+            <time class="italic detail-time fs-3 text-gray" datetime="2012-2014">2012-2014</time>
+            <p>Associate of Arts in Graphic Design, San Francisco State University</p>
+          </li>
+          <li>
+            <time class="italic detail-time fs-3 text-gray" datetime="2008-2012">2008-2012</time>
+            <p>High School Diploma, Lincoln High School</p>
+          </li>
         </ul>
-      </address>
-    </li>
-    <li class="mb-10 w-half-offset">
-      <h3 class="mb-4 detail-title title-2">Education</h3>
-      <ul class="fs-2">
-        <li class="mb-4">
-          <time class="italic detail-time fs-3 text-gray" datetime="2014-2018">2014-2018</time>
-          <p>Bachelor of Science in Business Administration, University of California, Los Angeles</p>
-        </li>
-        <li class="mb-4">
-          <time class="italic detail-time fs-3 text-gray" datetime="2012-2014">2012-2014</time>
-          <p>Associate of Arts in Graphic Design, San Francisco State University</p>
-        </li>
-        <li>
-          <time class="italic detail-time fs-3 text-gray" datetime="2008-2012">2008-2012</time>
-          <p>High School Diploma, Lincoln High School</p>
-        </li>
-      </ul>
-    </li>
-    <li class="mb-10 w-half-offset">
-      <h3 class="mb-4 detail-title title-2">Work</h3>
-      <ul class="fs-2">
-        <li class="mb-4">
-          <time class="italic detail-time fs-3 text-gray" datetime="2018-2021">2018-2021</time>
-          <p>Marketing Manager, ABC Company</p>
-        </li>
-        <li class="mb-4">
-          <time class="italic detail-time fs-3 text-gray" datetime="2016-2018">2016-2018</time>
-          <p>Sales Associate, XYZ Corporation</p>
-        </li>
-        <li>
-          <time class="italic detail-time fs-3 text-gray" datetime="2014-2016">2014-2016</time>
-          <p>Customer Service Representative, QRS Inc.</p>
-        </li>
-      </ul>
-    </li>
-    <li class="w-half-offset">
-      <h3 class="mb-4 detail-title title-2">Awards</h3>
-      <p class="mb-3 fs-2">Best in Show Award, National Advertising Awards, 2020</p>
-      <p class="mb-3 fs-2">Honorable Mention, International Design Competition, 2019</p>
-      <p class="fs-2">People's Choice Award, Local Art Exhibition, 2018</p>
-    </li>
-  </ul>
-</main>
-<footer class="bg-black footer">
-  <ul class="text-center social-link">
-    <li><a href="#"><img src="/public/hex/week1/personalResume/fb.png" alt="vito's fb"></a></li>
-    <li><a href="#"><img src="/public/hex/week1/personalResume/instagram.png" alt="vito's instagram"></a></li>
-    <li><a href="#"><img src="/public/hex/week1/personalResume/line.png" alt="vito's line"></a></li>
-  </ul>
-</footer>
+      </li>
+      <li class="mb-10 w-half-offset">
+        <h3 class="mb-4 detail-title title-2">Work</h3>
+        <ul class="fs-2">
+          <li class="mb-4">
+            <time class="italic detail-time fs-3 text-gray" datetime="2018-2021">2018-2021</time>
+            <p>Marketing Manager, ABC Company</p>
+          </li>
+          <li class="mb-4">
+            <time class="italic detail-time fs-3 text-gray" datetime="2016-2018">2016-2018</time>
+            <p>Sales Associate, XYZ Corporation</p>
+          </li>
+          <li>
+            <time class="italic detail-time fs-3 text-gray" datetime="2014-2016">2014-2016</time>
+            <p>Customer Service Representative, QRS Inc.</p>
+          </li>
+        </ul>
+      </li>
+      <li class="w-half-offset">
+        <h3 class="mb-4 detail-title title-2">Awards</h3>
+        <p class="mb-3 fs-2">Best in Show Award, National Advertising Awards, 2020</p>
+        <p class="mb-3 fs-2">Honorable Mention, International Design Competition, 2019</p>
+        <p class="fs-2">People's Choice Award, Local Art Exhibition, 2018</p>
+      </li>
+    </ul>
+  </main>
+  <footer class="bg-black footer">
+    <ul class="text-center social-link">
+      <li><a href="#"><img src="/public/hex/week1/personalResume/fb.png" alt="vito's fb"></a></li>
+      <li><a href="#"><img src="/public/hex/week1/personalResume/instagram.png" alt="vito's instagram"></a></li>
+      <li><a href="#"><img src="/public/hex/week1/personalResume/line.png" alt="vito's line"></a></li>
+    </ul>
+  </footer>
+</div>
 </template>
 
 <style lang="sass" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Castoro:ital@0;1&display=swap')
 
 // -------------------------------------
-//   Global
+//   Root
 // -------------------------------------
-body
+.personalResumePage
   line-height: 1.5
   font-family: Castoro, Arial, Helvetica, sans-serif
-img
-  vertical-align: middle
+  img
+    vertical-align: middle
 
 // -------------------------------------
 //   Helper
--- pages/hex/week2/personalWebsitePage.vue
+++ pages/hex/week2/personalWebsitePage.vue
@@ -5,126 +5,128 @@ useSeoMeta({
 </script>
 
 <template>
-<header class="bg-black header">
-  <div class="container">
-    <!-- nav -->
-    <nav class="py-40 nav">
-      <ul class="flex items-center justify-between text-gray nav-list">
-        <li>WILLIAM LEE</li>
-        <li>BASED IN TAIWAN</li>
-        <li>CURRENTLY FRONT END ENGINEER AT HEXSCHOOL</li>
-        <li><a href="#">CONTACT ME</a></li>
-      </ul>
-    </nav>
-    <!-- banner -->
-    <div class="flex items-center gap-6 banner ">
-      <img src="/hex/week2/personalWebsitePage/people.png" alt="William's 個人黑白沙龍">
-      <div class="flex flex-col banner-info float-start">
-        <h1 class="text-white title-1">Hi <br />I’m a front-end<br /> developer <span class="block text-right text-danger h1">William</span></h1>
-        <button class="flex items-center self-end text-black bg-white">contact me <img src="/public/hex/week2/personalWebsitePage/arrow_outward.png" alt="contact arrow" aria-hidden="true"></button>
-      </div>
-    </div>
-  </div>
-</header>
-<main>
-  <!-- intro -->
-  <section class="intro py-160">
-    <div class="container flex gap-6">
-      <div class="flex flex-col text-white intro-info">
-        <h2 class="title-1">Introduction</h2>
-        <p class="mt-9 fs-2">Hi, I'm William Lee, a Front-End Developer with a passion for building beautiful and responsive websites that provide an optimal user experience.</p>
-        <div class="flex items-start gap-4 mt-8">
-          <img src="/public/hex/week2/personalWebsitePage/sparkle-sm.png" alt="" aria-hidden="true">
-          <img src="/public/hex/week2/personalWebsitePage/sparkle-sm.png" alt="" aria-hidden="true">
-          <img src="/public/hex/week2/personalWebsitePage/sparkle-sm.png" alt="" aria-hidden="true">
+<div class="personalWebsitePage">
+  <header class="bg-black header">
+    <div class="container">
+      <!-- nav -->
+      <nav class="py-40 nav">
+        <ul class="flex items-center justify-between text-gray nav-list">
+          <li>WILLIAM LEE</li>
+          <li>BASED IN TAIWAN</li>
+          <li>CURRENTLY FRONT END ENGINEER AT HEXSCHOOL</li>
+          <li><a href="#">CONTACT ME</a></li>
+        </ul>
+      </nav>
+      <!-- banner -->
+      <div class="flex items-center gap-6 banner ">
+        <img src="/hex/week2/personalWebsitePage/people.png" alt="William's 個人黑白沙龍">
+        <div class="flex flex-col banner-info float-start">
+          <h1 class="text-white title-1">Hi <br />I’m a front-end<br /> developer <span class="block text-right text-danger h1">William</span></h1>
+          <button class="flex items-center self-end text-black bg-white">contact me <img src="/public/hex/week2/personalWebsitePage/arrow_outward.png" alt="contact arrow" aria-hidden="true"></button>
         </div>
       </div>
-      <ul class="flex items-start gap-6">
-        <img src="/public/hex/week2/personalWebsitePage/sparkle.png" alt="" aria-hidden="true">
-        <img src="/public/hex/week2/personalWebsitePage/photographer.png" alt="William's 手持照像機工作局部特寫">
-        <img src="/public/hex/week2/personalWebsitePage/people-2.png" alt="William's 個人短髮正視前方黑白型像照">
-      </ul>
     </div>
-  </section>
-  <!-- about -->
-  <section class="container flex items-center justify-between about py-160">
-    <ol class="flex flex-wrap gap-6 about-skill">
-      <li class="flex flex-col gap-1 fs-3">
-        01
-        <h3>Attention to Detail</h3>
-        <p class="fs-normal">Meticulous code analysis and optimization.</p>
-      </li>
-      <li class="flex flex-col gap-1 fs-3">
-        02
-        <h3>Adaptability</h3>
-        <p class="fs-normal">Swift learning of new technologies.</p>
-      </li>
-      <li class="flex flex-col gap-1 fs-3">
-        03
-        <h3>Problem Solver</h3>
-        <p class="fs-normal">Identifying and resolving issues.</p>
-      </li>
-      <li class="flex flex-col gap-1 fs-3">
-        04
-        <h3>Team Player</h3>
-        <p class="fs-normal">Collaborates and shares ideas.</p>
-      </li>
-    </ol>
-    <div class="about-info">
-      <h2 class="title-1">About Me</h2>
-      <p class="fs-2 mt-9">With over 5 years of experience, I'm proficient in HTML, CSS, JavaScript, jQuery, and React. I have a strong understanding of user experience design and accessibility. I'm always learning new technologies and techniques to stay up-to-date in the field.</p>
-    </div>
-  </section>
-  <!-- contact -->
-  <section class="bg-blue-dark-linear contact py-96">
-    <div class="container flex items-center justify-between">
-      <h3 class="flex items-center gap-4 text-white contact-title title-1">CONTACT ME<img src="/public/hex/week2/personalWebsitePage/trending_flat_black_24.png" alt="" aria-hidden="true"></h3>
-      <ul class="flex gap-5 contact-social">
-        <li><a href="#"><img src="/public/hex/week2/personalWebsitePage/discord.png" alt="William's Discord"></a></li>
-        <li><a href="#"><img src="/public/hex/week2/personalWebsitePage/instagram.png" alt="William's Instagram"></a></li>
-        <li><a href="#"><img src="/public/hex/week2/personalWebsitePage/message.png" alt="William's Message"></a></li>
-      </ul>
-    </div>
-  </section>
-  <!-- projects -->
-  <section class="projects py-160">
-    <div class="container flex flex-col gap-4">
-      <h2 class="ml-auto title-1 projects-title">My Projects</h2>
-      <div class="flex items-start gap-6 projects-bd">
-        <img src="/public/hex/week2/personalWebsitePage/Rectangle 10.png" alt="">
-        <div class="flex flex-col gap-7">
-          <div class="flex gap-6 projects-intro">
-            <img class="self-start" src="/public/hex/week2/personalWebsitePage/Rectangle 15.png" alt="">
-            <div class="flex flex-col items-start justify-between">
-              <p>I've worked on a variety of websites, including e-commerce sites, blogs, and landing pages. I take pride in creating clean, efficient, and user-friendly code.</p>
-              <button class="flex items-center text-white bg-black button-lightMode">SEE MORE <img src="/public/hex/week2/personalWebsitePage/arrow_outward-white.png" alt="see more icon" aria-hidden="true"></button>
+  </header>
+  <main>
+    <!-- intro -->
+    <section class="intro py-160">
+      <div class="container flex gap-6">
+        <div class="flex flex-col text-white intro-info">
+          <h2 class="title-1">Introduction</h2>
+          <p class="mt-9 fs-2">Hi, I'm William Lee, a Front-End Developer with a passion for building beautiful and responsive websites that provide an optimal user experience.</p>
+          <div class="flex items-start gap-4 mt-8">
+            <img src="/public/hex/week2/personalWebsitePage/sparkle-sm.png" alt="" aria-hidden="true">
+            <img src="/public/hex/week2/personalWebsitePage/sparkle-sm.png" alt="" aria-hidden="true">
+            <img src="/public/hex/week2/personalWebsitePage/sparkle-sm.png" alt="" aria-hidden="true">
+          </div>
+        </div>
+        <ul class="flex items-start gap-6">
+          <img src="/public/hex/week2/personalWebsitePage/sparkle.png" alt="" aria-hidden="true">
+          <img src="/public/hex/week2/personalWebsitePage/photographer.png" alt="William's 手持照像機工作局部特寫">
+          <img src="/public/hex/week2/personalWebsitePage/people-2.png" alt="William's 個人短髮正視前方黑白型像照">
+        </ul>
+      </div>
+    </section>
+    <!-- about -->
+    <section class="container flex items-center justify-between about py-160">
+      <ol class="flex flex-wrap gap-6 about-skill">
+        <li class="flex flex-col gap-1 fs-3">
+          01
+          <h3>Attention to Detail</h3>
+          <p class="fs-normal">Meticulous code analysis and optimization.</p>
+        </li>
+        <li class="flex flex-col gap-1 fs-3">
+          02
+          <h3>Adaptability</h3>
+          <p class="fs-normal">Swift learning of new technologies.</p>
+        </li>
+        <li class="flex flex-col gap-1 fs-3">
+          03
+          <h3>Problem Solver</h3>
+          <p class="fs-normal">Identifying and resolving issues.</p>
+        </li>
+        <li class="flex flex-col gap-1 fs-3">
+          04
+          <h3>Team Player</h3>
+          <p class="fs-normal">Collaborates and shares ideas.</p>
+        </li>
+      </ol>
+      <div class="about-info">
+        <h2 class="title-1">About Me</h2>
+        <p class="fs-2 mt-9">With over 5 years of experience, I'm proficient in HTML, CSS, JavaScript, jQuery, and React. I have a strong understanding of user experience design and accessibility. I'm always learning new technologies and techniques to stay up-to-date in the field.</p>
+      </div>
+    </section>
+    <!-- contact -->
+    <section class="bg-blue-dark-linear contact py-96">
+      <div class="container flex items-center justify-between">
+        <h3 class="flex items-center gap-4 text-white contact-title title-1">CONTACT ME<img src="/public/hex/week2/personalWebsitePage/trending_flat_black_24.png" alt="" aria-hidden="true"></h3>
+        <ul class="flex gap-5 contact-social">
+          <li><a href="#"><img src="/public/hex/week2/personalWebsitePage/discord.png" alt="William's Discord"></a></li>
+          <li><a href="#"><img src="/public/hex/week2/personalWebsitePage/instagram.png" alt="William's Instagram"></a></li>
+          <li><a href="#"><img src="/public/hex/week2/personalWebsitePage/message.png" alt="William's Message"></a></li>
+        </ul>
+      </div>
+    </section>
+    <!-- projects -->
+    <section class="projects py-160">
+      <div class="container flex flex-col gap-4">
+        <h2 class="ml-auto title-1 projects-title">My Projects</h2>
+        <div class="flex items-start gap-6 projects-bd">
+          <img src="/public/hex/week2/personalWebsitePage/Rectangle 10.png" alt="">
+          <div class="flex flex-col gap-7">
+            <div class="flex gap-6 projects-intro">
+              <img class="self-start" src="/public/hex/week2/personalWebsitePage/Rectangle 15.png" alt="">
+              <div class="flex flex-col items-start justify-between">
+                <p>I've worked on a variety of websites, including e-commerce sites, blogs, and landing pages. I take pride in creating clean, efficient, and user-friendly code.</p>
+                <button class="flex items-center text-white bg-black button-lightMode">SEE MORE <img src="/public/hex/week2/personalWebsitePage/arrow_outward-white.png" alt="see more icon" aria-hidden="true"></button>
+              </div>
+            </div>
+            <div class="flex items-start gap-6 projects-photo">
+              <img src="/public/hex/week2/personalWebsitePage/Rectangle 14.png" alt="">
+              <img src="/public/hex/week2/personalWebsitePage/Rectangle 29.png" alt="">
             </div>
           </div>
-          <div class="flex items-start gap-6 projects-photo">
-            <img src="/public/hex/week2/personalWebsitePage/Rectangle 14.png" alt="">
-            <img src="/public/hex/week2/personalWebsitePage/Rectangle 29.png" alt="">
-          </div>
         </div>
       </div>
-    </div>
-  </section>
-</main>
-<footer class="py-40 text-center text-white bg-black footer">all rights reserved @hexschool2023</footer>
+    </section>
+  </main>
+  <footer class="py-40 text-center text-white bg-black footer">all rights reserved @hexschool2023</footer>
+</div>
 
 </template>
 
-<style lang="sass">
+<style lang="sass" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Bruno+Ace+SC&display=swap')
 
 // use reset, tailwind css
 // -------------------------------------
-//   Global
+//   Root
 // -------------------------------------
-body
+.personalWebsitePage
   line-height: 1.5
   font-family: "Bruno Ace SC", sans-serif
-img, svg
-  vertical-align: middle
+  img
+    vertical-align: middle
 
 // -------------------------------------
 //   Helper
