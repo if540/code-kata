c85776718736796e70674ad7e30c804297a9ce68
Kerwin <if5400@gmail.com>
2024-07-15 16:18:31 +0800

    update contetn page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,6 +1,19 @@
 <template>
   <main>
     <!-- ContentDoc returns content for `$route.path` by default or you can pass a `path` prop -->
-    <ContentDoc />
+    <ContentDoc>
+      <template v-slot="{ doc }">
+        <article>
+          <h1>{{ doc.title }}</h1>
+          <ContentRenderer :value="doc" />
+        </article>
+      </template>
+      <template #not-found>
+        <h1>Document not found</h1>
+      </template>
+      <template #empty>
+        <h1>Document is empty</h1>
+      </template>
+    </ContentDoc> 
   </main>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,22 +2,11 @@
 useSeoMeta({
   title: "Index Page"
 })
-
-// const article =  await queryContent()
-//   .where({ _type: { $ne: 'md' } })
-//   .sort({ date: -1 })
-//   .limit(1)
-//   .find()
-// console.log('article',article)
-
-const { data } = await useAsyncData('hello', () => queryContent('/hello').findOne())
-
 </script>
 
 <template>
   <div>
     Page: Index
-    <ContentRenderer :value="data" />
   </div>
 </template>
 
