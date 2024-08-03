d82b07dc247004ae3dfc899d70a8293798363589
Kerwin <if5400@gmail.com>
2024-08-03 15:20:23 +0800

    新增 shadcn-vue ui lib、nuxt icon、nuxt lucide icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,31 +3,51 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   compatibilityDate: "2024-07-03",
   app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/code-kata/' : '/',
-    buildAssetsDir: '/static/',
+    baseURL: process.env.NODE_ENV === "production" ? "/code-kata/" : "/",
+    buildAssetsDir: "/static/",
     rootAttrs: {
-      id: "app" 
+      id: "app",
     },
   },
-  css: [
-    "~/assets/reset.css"
+  css: ["~/assets/reset.css"],
+  modules: [
+    "@nuxt/content",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxtjs/color-mode",
+    "@nuxt/icon",
+    "nuxt-lucide-icons"
   ],
-  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
   ssr: false,
   content: {
     api: {
-      baseURL: '/api/_content'
+      baseURL: "/api/_content",
     },
     documentDriven: true,
     highlight: {
-      theme: 'github-light'
+      theme: "github-light",
     },
     navigation: {
-      fields: ['author', 'publishedAt']
+      fields: ["author", "publishedAt"],
     },
     experimental: {
-      clientDB: true
-    }
+      clientDB: true,
+    },
   },
-})  
-  
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+  icon: {
+    serverBundle: {
+      remote: "jsdelivr", // 'unpkg' or 'github-raw', or a custom function
+    },
+  },
+});
